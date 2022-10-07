739da9caa6fb06b0ec5b8e52323007c1e841f932
Balakishi <balakishi.aliyev@gmail.com>
2022-10-07 22:33:48 +0400

    Add project files.

---
+++ HumanBodyİndex/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace HumanBodyİndex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label_result = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_result = new System.Windows.Forms.Button();
+            this.label_bmi = new System.Windows.Forms.Label();
+            this.textBox_h = new System.Windows.Forms.TextBox();
+            this.textBox_w = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_result
+            // 
+            this.label_result.AutoSize = true;
+            this.label_result.Font = new System.Drawing.Font("Nivea", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_result.Location = new System.Drawing.Point(888, 359);
+            this.label_result.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_result.Name = "label_result";
+            this.label_result.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label_result.Size = new System.Drawing.Size(0, 28);
+            this.label_result.TabIndex = 27;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Nivea", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(723, 352);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 36);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Result:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nivea", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(723, 303);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(255, 36);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Body Mass index:";
+            // 
+            // button_result
+            // 
+            this.button_result.Font = new System.Drawing.Font("Nautilus Pompilius", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_result.Location = new System.Drawing.Point(666, 231);
+            this.button_result.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.button_result.Name = "button_result";
+            this.button_result.Size = new System.Drawing.Size(406, 47);
+            this.button_result.TabIndex = 24;
+            this.button_result.Text = "The BMI of this person(please click):";
+            this.button_result.UseVisualStyleBackColor = true;
+            this.button_result.Click += new System.EventHandler(this.button_result_Click);
+            // 
+            // label_bmi
+            // 
+            this.label_bmi.AutoSize = true;
+            this.label_bmi.Font = new System.Drawing.Font("Nivea", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_bmi.Location = new System.Drawing.Point(1032, 311);
+            this.label_bmi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_bmi.Name = "label_bmi";
+            this.label_bmi.Size = new System.Drawing.Size(0, 28);
+            this.label_bmi.TabIndex = 23;
+            // 
+            // textBox_h
+            // 
+            this.textBox_h.Font = new System.Drawing.Font("Nivea", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_h.Location = new System.Drawing.Point(961, 156);
+            this.textBox_h.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.textBox_h.Multiline = true;
+            this.textBox_h.Name = "textBox_h";
+            this.textBox_h.Size = new System.Drawing.Size(200, 30);
+            this.textBox_h.TabIndex = 22;
+            // 
+            // textBox_w
+            // 
+            this.textBox_w.Font = new System.Drawing.Font("Nivea", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_w.Location = new System.Drawing.Point(961, 76);
+            this.textBox_w.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.textBox_w.Multiline = true;
+            this.textBox_w.Name = "textBox_w";
+            this.textBox_w.Size = new System.Drawing.Size(200, 30);
+            this.textBox_w.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkOrange;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Nivea", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(617, 156);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(332, 30);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Please enter value of height:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkOrange;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Nivea", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(617, 76);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(336, 30);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Please enter value of wieght:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::HumanBodyİndex.Properties.Resources.istockphoto_1039931118_612x612;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1172, 450);
+            this.Controls.Add(this.label_result);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button_result);
+            this.Controls.Add(this.label_bmi);
+            this.Controls.Add(this.textBox_h);
+            this.Controls.Add(this.textBox_w);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_result;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_result;
+        private System.Windows.Forms.Label label_bmi;
+        private System.Windows.Forms.TextBox textBox_h;
+        private System.Windows.Forms.TextBox textBox_w;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HumanBodyİndex/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HumanBodyİndex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double h, w,result_bmi;
+        private void button_result_Click(object sender, EventArgs e)
+        {
+            try { 
+
+         //i did that in this if block, If you entered string, zero or you have not entered value you will notified by system
+ if (!Double.TryParse(textBox_w.Text, out w) || !Double.TryParse(textBox_h.Text, out w) ||
+ textBox_h.Text.Contains("-") || textBox_w.Text.Contains("-")|| textBox_h.Text == "0" || textBox_w.Text == "0")
+            
+            {
+                DialogResult result = MessageBox.Show("The values of weight and height of human not be this value or you have not entered the value",
+  "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
+            /* in this else blokc, if you not entered string, zero value application calculate BMI but there are two case: heigt 
+             * maybe entered with cm and height entered with metr. Therefore, i write two case. My application calculate also two case. 
+             * For example, you enterd 170cm heigt or 1,7 metr heigt. It calculate two case
+             * 
+             */
+                else
+                {
+                    w = Convert.ToDouble(textBox_w.Text);
+                    h = Convert.ToDouble(textBox_h.Text);
+                    if (h < 3)
+                    {
+                        result_bmi = (double)((int)((w / Math.Pow(h, 2)) * 100)) / 100;
+                        label_bmi.Text = Convert.ToString(result_bmi);
+                    }
+                    else
+                    {
+                        h = h / 100;
+                        result_bmi = (double)((int)((w / Math.Pow(h, 2)) * 100)) / 100;
+                        label_bmi.Text = Convert.ToString(result_bmi);
+                    }
+                    #region "Result block"
+                    if (result_bmi < 18.5)
+                    {
+                        label_result.Text = "You are underweight";
+                    }
+                    else if (result_bmi > 18.5 && result_bmi < 24.9)
+                    {
+                        label_result.Text = "You are normal";
+                    }
+                    else if (result_bmi > 25 && result_bmi < 29.9)
+                    {
+                        label_result.Text = "You are overweight";
+                    }
+                    else if (result_bmi > 30 && result_bmi < 34.9)
+                    {
+                        label_result.Text = "You are obese";
+                    }
+                    else if (result_bmi > 35)
+                    {
+                        label_result.Text = "You are extremly obese";
+                    }
+                    #endregion
+                }
+            }
+            catch
+            {
+                
+            }
+        }
+    }
+}
